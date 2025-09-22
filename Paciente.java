6c0dcc133649536958c44a78470e2ef1628506f4
Douglas Michel <d.michel.dev@gmail.com>
2025-09-22 18:57:36 -0500

    feat: paciente na main

--- Paciente.java
+++ Paciente.java
@@ -1,14 +1,34 @@
-import java.sql.Date;
+import java.time.LocalDate;
+
 public class Paciente {
     private Long id;
     private String nome;
     private String email;
     private String telefone;
-    private Date dataNascimento;
+    private LocalDate dataNascimento;
     private String grupoSanguineo;
     private String sexo;
     private String cep;
     private String endereco;
     private String cidade;
     private String estado;
+
+    public Paciente(Long id, String nome, String email, String telefone, LocalDate dataNascimento, String grupoSanguineo, String sexo, String cep, String endereco, String cidade, String estado) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+        this.grupoSanguineo = grupoSanguineo;
+        this.sexo = sexo;
+        this.cep = cep;
+        this.endereco = endereco;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    String obterPaciente() {
+        return String.format("ID: " + id + "\n" + "NOME: " + nome + "\n" + "EMAIL: " + email + "\n" + "TELEFONE: " + telefone + "\n" + "DATA DE NASCIMENTO: " + dataNascimento + "\n"
+        + "GRUPO SANGUÍNEO: " + grupoSanguineo + "\n" + "SEXO: " + sexo + "\n" + "CEP: " + cep + "\n" + "ENDEREÇO: " + endereco + "\n" + "CIDADE: " + cidade + "\n" + "ESTADO: " + estado);
+    }
 }
